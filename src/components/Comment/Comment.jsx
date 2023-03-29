e9c3bfff934feeb652b59a7637dfd9558e836f3a
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-29 23:59:18 +0530

    feat: added functions for comment score counter

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -3,12 +3,39 @@ import { CommentHeader } from './CommentHeader';
 import { CommentScoreCounter } from './CommentScoreCounter';
 
 import styles from './comment.module.scss';
+import { useState } from 'react';
 
 export function Comment({ data }) {
     const { content, score, user, createdAt } = data;
+
+    const [scoreCount, setScoreCount] = useState(score);
+
+    const [canIncrease, setCanIncrease] = useState(true);
+    const [canDecrease, setCanDecrease] = useState(true);
+
+    function handleIncreaseScore() {
+        if (canIncrease) {
+            setScoreCount((score) => (score += 1));
+            setCanIncrease(!canIncrease);
+            setCanDecrease(true);
+        }
+    }
+
+    function handleDecreaseScore() {
+        if (canDecrease) {
+            setScoreCount((score) => (score -= 1));
+            setCanDecrease(!canDecrease);
+            setCanIncrease(true);
+        }
+    }
+
     return (
         <div className={styles.comment_container}>
-            <CommentScoreCounter score={score} />
+            <CommentScoreCounter
+                score={scoreCount}
+                handleIncreaseScore={handleIncreaseScore}
+                handleDecreaseScore={handleDecreaseScore}
+            />
             <div className={styles.comment_data}>
                 <CommentHeader
                     imgSrc={user.image.png}
--- src/components/Comment/CommentScoreCounter.jsx
+++ src/components/Comment/CommentScoreCounter.jsx
@@ -2,14 +2,14 @@ import styles from './comment.module.scss';
 import { ReactComponent as IconMinus } from '../../assets/images/icon-minus.svg';
 import { ReactComponent as IconPlus } from '../../assets/images/icon-plus.svg';
 
-export function CommentScoreCounter({ score = 0 }) {
+export function CommentScoreCounter({ score = 0, handleIncreaseScore, handleDecreaseScore }) {
     return (
         <div className={styles.comment_score_container}>
-            <button className={styles.comment_score_button}>
+            <button className={styles.comment_score_button} onClick={handleIncreaseScore}>
                 <IconPlus />
             </button>
             <p className={styles.comment_score}>{score}</p>
-            <button className={styles.comment_score_button}>
+            <button className={styles.comment_score_button} onClick={handleDecreaseScore}>
                 <IconMinus />
             </button>
         </div>
