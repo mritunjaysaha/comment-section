e9e9a49c786e0ea83d764d663c436a70d48ce9e2
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-30 00:51:22 +0530

    refactor: updated commentTextArea user image getting the user image from the redux store

--- src/components/CommentTextArea/CommentTextArea.jsx
+++ src/components/CommentTextArea/CommentTextArea.jsx
@@ -1,11 +1,14 @@
+import { useSelector } from 'react-redux';
 import { Button } from '../Atoms/Button/Button';
 
 import styles from './commentTextArea.module.scss';
 
-export function CommentTextArea({ userImg, handleButtonClick, buttonText = 'send' }) {
+export function CommentTextArea({ handleButtonClick, buttonText = 'send' }) {
+    const { currentUser } = useSelector((state) => state.comment);
+
     return (
         <div className={`${styles.comment_container} ${styles.comment_text_area_container}`}>
-            <img src={userImg} className={styles.user_image} />
+            <img src={currentUser.image.png} className={styles.user_image} />
             <textarea className={styles.textarea} placeholder='Add a comment...' rows={4} />
             <Button onClick={handleButtonClick}>{buttonText}</Button>
         </div>
